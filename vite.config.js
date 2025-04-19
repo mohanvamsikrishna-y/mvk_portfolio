bf74cc3ae8b52384f164ad3489fcfdea01ab1cf6
Mohan Vamsi Krishna <myanamadala@binghamton.edu>
2025-04-19 00:57:07 -0400

    Set homepage and base for github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/mvk-portfolio/', // 👈 IMPORTANT
+  base: '/mvk_portfolio/', // 👈 IMPORTANT
   plugins: [react()],
 });
